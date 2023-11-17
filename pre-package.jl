cb57e943cc7e7ceef47bc77c6d5507c2b7857caa
Heptazhou <zhou@0h7z.com>
2023-11-17 01:49:09 +0000

    Update

--- pre-package.jl
+++ pre-package.jl
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-((d, f) -> write(f, replace(read(d * f, String), "\t" => "")))("out/", "main.js")
+((d, f) -> write(f, replace(read(d * f, String), r"^\t+"m => "")))("out/", "main.js")
 
 const dir = "node_modules/"
 const mod = ["@rauschma/stringio", "diff", "untildify", "util"]
